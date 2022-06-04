fa830b3fcb358c4816f93906d0c255dd7434f1ae
oscarsbDeveloper <89110094+oscarsbDeveloper@users.noreply.github.com>
2022-06-03 19:00:55 -0600

    Commit_01_Index_Home_Creados

---
+++ GoldenToad/src/main/java/com/goldentoad/GoldenToadApplication.java
@@ -0,0 +1,13 @@
+package com.goldentoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenToadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldenToadApplication.class, args);
+	}
+
+}
---
+++ GoldenToad/src/test/java/com/goldentoad/GoldenToadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldentoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenToadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
