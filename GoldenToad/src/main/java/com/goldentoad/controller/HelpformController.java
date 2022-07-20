f432196283486f6e390f89cc005da5d10b018ce7
LuciMadri <89278285+LuciMadri@users.noreply.github.com>
2022-07-20 16:43:46 -0600

    arreglos de projects y crear html de helpform
    da error edit y delete

--- GoldenToad/src/main/java/com/goldentoad/controller/HelpformController.java
+++ GoldenToad/src/main/java/com/goldentoad/controller/HelpformController.java
@@ -31,103 +31,101 @@ public class HelpformController {
 
     @Autowired
     private INewsService newsService;
-    
+
     @Autowired
-     private IProjectService projectService;
+    private IProjectService projectService;
 
-    /*El GetMapping crea la ruta, entonces al escribir localhost/persona 
-    nos ejecuta el metodo
-     */
-
-    @GetMapping("/helpform")
-
-    //Puede ser cualquier nombre en vez de INDEX
-    /*Aqui le pasamos un objeto de tipo model, ya que le pasamos informacion
-    que puede ser sustituida ya que proviene de la bd, entonces la forma en la
-    que le pasamos esos valores es por el objeto de tipo model*/
-    public String index(Model model) {
-        /*Aqui creamos una lista y le pasamos la informacion por medio del metodo
-          getAllPersona de nuestra clase interface.*/
-        List<Helpform> listHelpform = helpformService.getAllHelpform();
-        
-
-        /*Aqui, en nuestro html donde se encuentre la palabra titulo lo va a 
-          sustituir por tabla persona*/
-        model.addAttribute("titulo", "Helpform Table");
-
-        /*Aqui, en nuestro html donde se encuentre la palabra personas lo va a 
-         sustituir por la listaPersona*/
-        model.addAttribute("helpform", listHelpform);
-
-        //Aqui retornamos un html que se llama personas.
-        return "helpform";
-    }
     
-    //==========================================================================
-    //===============================NEWS=======================================
-    //==========================================================================
-    @GetMapping("/newsN")
-    public String news(Model model){
-        List<News>listNews = newsService.getAllNews();
-        model.addAttribute("titulo", "News Table");
-        model.addAttribute("news", listNews);
-        return "newsN";
-    }
-    
-    //Crear una persona nueva CRUD
-    @GetMapping("/crearNews")
-    public String crearNews(Model model){
-        model.addAttribute("news",new News());
-        return "crearNews";
-    } 
-    
-    @PostMapping("/save")
-    public String guardarNews(@ModelAttribute News news){
-        newsService.saveNews(news);
-        return "redirect:/newsN";
-    }
-    
-    @GetMapping("/editNews/{id_news}")
-    public String editarNews(@PathVariable("id_news")Integer idNews,Model model)
-    {
-        News news = newsService.getNewsById(idNews);
-        model.addAttribute("news",news);
-        return "crearNews";
-    }
-    
-    @GetMapping("/delete/{id_news}")
-    public String eliminarNews(@PathVariable("id_news")Integer idNews){
-        newsService.delete(idNews);
-        return "redirect:/newsN";
-    } 
     
     //==========================================================================
     //--------------------------- Helpform -------------------------------------
     //==========================================================================
+    /*El GetMapping crea la ruta, entonces al escribir localhost/persona 
+    nos ejecuta el metodo
+     */
+    @GetMapping("/helpform")
+    public String index(Model model) {
+        List<Helpform> listHelpform = helpformService.getAllHelpform();
+        model.addAttribute("titulo", "Helpform Table");
+        model.addAttribute("helpform", listHelpform);
+        return "helpform";
+    }
     
+    @GetMapping("/helpformN")
+    public String createHelpform(Model model){
+        List<Userform> listUsers = userformService.listUser();
+        model.addAttribute("helpform", new Helpform());       
+        model.addAttribute("users", listUsers);
+        return "crearHelpform";
+    }
+
     @GetMapping("/saveH")
-    public String saveHelpform(@ModelAttribute Helpform helpform){
+    public String saveHelpform(@ModelAttribute Helpform helpform) {
         //Con esto lo guardo en la bd
         helpformService.saveHelpform(helpform);
-        
+
         //Aqui quiero que me redirija a otro get mapping
         return "redirect:/helpform";
     }
-    
-    @GetMapping("/editHelpform/{id}")
-    public String editHelpform(@PathVariable("id") Long idHelpform, Model model){
-        Helpform helpform = helpformService.getHelpformById(idHelpform);
+
+    @GetMapping("/editHelpform/{id_help}")
+    public String editHelpform(@PathVariable("id_help") Long idHelp, Model model) {
+        Helpform helpform = helpformService.getHelpformById(idHelp);
         List<Userform> listUserform = userformService.listUser();
         model.addAttribute("helpform", helpform);
         model.addAttribute("userform", listUserform);
-        return "create";
-    } 
+        return "crearHelpform";
+        
+        
+    }
     
+    @GetMapping("/deleteHelpform/{id_help}")
+    public String eliminarHelpform(@PathVariable("id_help") Integer idhelp) {
+        projectService.delete(idhelp);
+        return "redirect:/helpform";
+    }
+
+    //==========================================================================
+    //===============================NEWS=======================================
+    //==========================================================================
+    @GetMapping("/newsN")
+    public String news(Model model) {
+        List<News> listNews = newsService.getAllNews();
+        model.addAttribute("titulo", "News Table");
+        model.addAttribute("news", listNews);
+        return "newsN";
+    }
+
+    //Crear una persona nueva CRUD
+    @GetMapping("/crearNews")
+    public String crearNews(Model model) {
+        model.addAttribute("news", new News());
+        return "crearNews";
+    }
+
+    @PostMapping("/save")
+    public String guardarNews(@ModelAttribute News news) {
+        newsService.saveNews(news);
+        return "redirect:/newsN";
+    }
+
+    @GetMapping("/editNews/{id_news}")
+    public String editarNews(@PathVariable("id_news") Integer idNews, Model model) {
+        News news = newsService.getNewsById(idNews);
+        model.addAttribute("news", news);
+        return "crearNews";
+    }
+
+    @GetMapping("/delete/{id_news}")
+    public String eliminarNews(@PathVariable("id_news") Integer idNews) {
+        newsService.delete(idNews);
+        return "redirect:/newsN";
+    }
+
     //==========================================================================
     //--------------------------- Projects -------------------------------------
     //==========================================================================
-    
-     @GetMapping("/projects")
+    @GetMapping("/projects")
     public String project(Model model) {
         List<Project> listProject = projectService.getAllProject();
         model.addAttribute("titulo", "Projects Table");
@@ -136,36 +134,35 @@ public class HelpformController {
         //Aqui retornamos un html que se llama personas.
         return "projects";
     }
-    
+
     //Crear una persona nueva CRUD
     @GetMapping("/projectN")
-    public String crearProject(Model model){
-        model.addAttribute("projects",new Project());
+    public String crearProject(Model model) {
+        model.addAttribute("projects", new Project());
         return "crearProjects";
-    } 
-    
+    }
+
     @PostMapping("/saveProject")
-    public String saveProject(@ModelAttribute Project project){
+    public String saveProject(@ModelAttribute Project project) {
         //Con esto lo guardo en la bd
         projectService.saveProject(project);
-        
+
         //Aqui quiero que me redirija a otro get mapping
         return "redirect:/projects";
     }
-    
+
     // Aqui en editar solo se esta usando project
     @GetMapping("/editProject/{id_projects}")
-    public String editProject(@PathVariable("id_projects") Integer idProject, Model model){
+    public String editProject(@PathVariable("id_projects") Integer idProject, Model model) {
         Project project = projectService.getProjectById(idProject);
         model.addAttribute("projects", project);
         return "crearProjects";
-    } 
-    
-    
+    }
+
     @GetMapping("/deleteProject/{id_projects}")
     public String eliminarProject(@PathVariable("id_projects") Integer idProject) {
-        projectService.delete(idProject); 
+        projectService.delete(idProject);
         return "redirect:/projects";
     }
-    
+
 }
