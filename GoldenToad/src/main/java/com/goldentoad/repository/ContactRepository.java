ee67218b1ab2cc5c57856eb3994aa0dd55583093
LuciMadri <89278285+LuciMadri@users.noreply.github.com>
2022-08-22 20:56:32 -0600

    solucion de error contact

--- GoldenToad/src/main/java/com/goldentoad/repository/ContactRepository.java
+++ GoldenToad/src/main/java/com/goldentoad/repository/ContactRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ContactRepository extends CrudRepository<Contact, Integer>{
-        Contact findByTitle(String title);
+        Contact findByName(String name);
 }
